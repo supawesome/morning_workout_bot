6fe0538e6061bc899b3abf4e1ef902e8953fc123
Aleksandr Shoyko <aleksandr.shoyko@gmail.com>
2023-04-11 23:04:25 +0700

    fix .gitignore

---
+++ configs/message_config.py
@@ -0,0 +1,19 @@
+HELP_MESSAGE = (
+    'Just tap on the dice and get a set of random exercises! \n'
+    'Each exercise belongs to different major muscle group (upper, middle or lower body) \n \n'
+    'Why workout is pseudo random? Procs of special events are sampled '
+    'from Pseudo-Random Distribution (like random-based abilities in Dota 2) \n'
+    'For more information on how this works, check out this link: '
+    'https://github.com/supawesome/PRD'
+)
+
+CHILL_EVENT_MESSAGE = (
+    "WOW! You have rolled rare Chill event! "
+    'No need to do these exercises for today!'
+)
+
+DOUBLE_EVENT_MESSAGE = (
+    "BOOM! You have rolled the rare Double event! "
+    "Do TWICE as many reps as usual for each exercise! \n \n"
+)
+
